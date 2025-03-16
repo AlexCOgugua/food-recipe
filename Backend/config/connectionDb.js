7a903564db56c53d74a2383676abd7c5034001ac
Alex Ogugua <oguguaalexc@gmail.com>
2025-03-16 19:33:01 +0100

    updated with cors middeware

--- Backend/config/connectionDb.js
+++ Backend/config/connectionDb.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 
 const connectDb = async() => {
-    await mongoose.connect(process.env.CONNECTION_STRING, {dbName: 'foodRecipe'})
+    await mongoose.connect(process.env.CONNECTION_STRING, 
+        {   dbName: 'foodRecipe', 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true, 
+            ssl: true,
+        }
+    )
     .then(()=>console.log("Database connected"))
 };
 
