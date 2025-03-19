95b380e8cc6e62ffec3814736fab207fdfc83cf4
Alex Ogugua <oguguaalexc@gmail.com>
2025-03-19 12:05:01 +0100

    reverted to server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,11 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 connectDb();
 app.use(express.json());
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.static("public"))
 
 
